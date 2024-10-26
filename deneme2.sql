07d86b4aab8b120dfa48971d39770e28df60b235
Yusuf Uygur <tarihte1110@gmail.com>
2024-10-26 12:33:35 +0300

    Revert "main_database"
    This reverts commit 1d3d92a15820f7fae4f2535f5dfb5bd703c135b4.

--- deneme2.sql
+++
@@ -1,6 +0,0 @@
---insert into car (id,marka,model) values (1,'ford','focus')
---insert into car (id,marka,model) values (3,'BMW','i5')
---select id,marka,model from car
---select model from car
---select * from car
-select * from car where marka='ford' and id=1 or model='focus'
