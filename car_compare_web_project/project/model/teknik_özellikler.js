e0ef7ffa86aa1b573254b054f9903113d3d6ce11
yasar12 <220542008@firat.edu.tr>
2024-11-11 16:05:15 +0300

    orm models added

---
+++ car_compare_web_project/project/model/teknik_özellikler.js
@@ -0,0 +1,64 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../configg');
+
+const TechnicalSpecification = sequelize.define('TechnicalSpecification', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    year: {
+      type: DataTypes.INTEGER
+    },
+    price: {
+      type: DataTypes.NUMERIC
+    },
+    horsepower: {
+      type: DataTypes.INTEGER
+    },
+    weight: {
+      type: DataTypes.INTEGER
+    },
+    top_speed: {
+      type: DataTypes.NUMERIC(5, 2)
+    },
+    drivetrain: {
+      type: DataTypes.STRING(10)
+    },
+    braking: {
+      type: DataTypes.NUMERIC(3, 1)
+    },
+    acceleration: {
+      type: DataTypes.NUMERIC(3, 1)
+    },
+    handling: {
+      type: DataTypes.NUMERIC(3, 1)
+    },
+    performance_index: {
+      type: DataTypes.INTEGER
+    },
+    zero_to_sixty: {
+      type: DataTypes.NUMERIC(4, 2)
+    },
+    zero_to_hundred: {
+      type: DataTypes.NUMERIC(4, 2)
+    },
+    weight_distribution: {
+      type: DataTypes.STRING(10)
+    },
+    plb_ratio: {
+      type: DataTypes.NUMERIC(4, 3)
+    },
+    release_date: {
+      type: DataTypes.DATE
+    },
+    group_type: {
+      type: DataTypes.STRING(50)
+    }
+  }, {
+    tableName: 'technicalSpecification',
+    timestamps: false
+  });
+  
+  module.exports = TechnicalSpecification;
+  
