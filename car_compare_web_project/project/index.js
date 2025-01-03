c128e563b838639dfda3eb25e0f736d1c5bcb7d9
yasar12 <220542008@firat.edu.tr>
2025-01-03 21:31:01 +0300

    update

--- car_compare_web_project/project/index.js
+++ car_compare_web_project/project/index.js
@@ -3,17 +3,32 @@ const app = express();
 const path = require('path');
 const usersRouters = require('./routes/usersRoutes');
 const adminRouters = require('./routes/AdminRoutes');
-
+const session = require('express-session');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'private')));
 app.use(express.static('node_modules'));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'public/views/ejs'));
+app.set('views', [
+  path.join(__dirname, 'public/views/ejs'), // Kullanıcı için
+  path.join(__dirname, 'private/views/ejs') // Admin için
+]);
 
 
+app.use(
+  session({
+    secret: 'gizliAnahtar',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false ,
+      maxAge: 24 * 60 * 60 * 1000 
+    }
+  })
+);
 
-app.use(usersRouters);
+app.use('/admin', adminRouters);
+app.use('/', usersRouters);
 
 
 app.listen(3000, () => {
--- car_compare_web_project/project/routes/usersRoutes.js
+++ car_compare_web_project/project/routes/usersRoutes.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const userrepository = require("../repositories/userRepository");
-
+const axios = require('axios');
 
 function errorHandler(err, req, res, next) {
     console.error('Error Middleware:', err.stack);
@@ -34,6 +34,9 @@ next();
   });
 
 
+
+
+  //ana sayfayı yükler
 route.get("/car_compare", async (req, res, next) => {
     try {
        
@@ -46,7 +49,7 @@ route.get("/car_compare", async (req, res, next) => {
         next(error); 
     }
 });
-
+//Seçilmiş arabaları resultgpt/get sayfasına yönlendirir
 route.post("/car_compare", async (req, res, next) => {
     try {
         const { marka } = req.body; 
@@ -64,128 +67,7 @@ route.post("/car_compare", async (req, res, next) => {
 });
 
 
-/*
-route.post('/car_compare/result', async (req, res, next) => {
-    try {
-        console.log(req.body);
-        const arabalar = req.body;
-
-        const car1 = arabalar.model1;
-        const car2 = arabalar.model2;
-        console.log(car1,car2);
-        await userrepository.send_compared_cars(car1,car2);
-        const carfull1 = arabalar.marka ;
-        const carfull2 = arabalar.marka ;
-
-        res.redirect(`/car_compare/result?carname1=${carfull1}&carname2=${carfull2}`);
-    } catch (error) {
-        next(error); 
-    }
-});
-*/
-
-/*
-route.get('/car_compare/result', async (req, res, next) => {
-    try {
-       
-        const { marka1, model1, marka2, model2 } = req.query;
-
-        const carSpec1 = await userrepository.getmodelid(model1);
-        const carSpec2 = await userrepository.getmodelid(model2);
-
-        const carDetails1 = await userrepository.getCarsByIds(carSpec1);
-        const carDetails2 = await userrepository.getCarsByIds(carSpec2);
-
-        
-        const car1 = carDetails1[0];
-        const car2 = carDetails2[0];
-        await userrepository.send_compared_cars(model1,model2);
-        const features = [
-            { key: 'year', name: 'Year' },
-            { key: 'price', name: 'Price' },
-            { key: 'horsepower', name: 'Horsepower' },
-            { key: 'weight', name: 'Weight' },
-            { key: 'top_speed', name: 'Top Speed' },
-            { key: 'drivetrain', name: 'Drivetrain' },
-            { key: 'braking', name: 'Braking (m/s²)' },
-            { key: 'acceleration', name: 'Acceleration (m/s²)' },
-            { key: 'handling', name: 'Handling' },
-            { key: 'performance_index', name: 'Performance Index' },
-            { key: 'zero_to_sixty', name: '0-60 mph (s)' },
-            { key: 'zero_to_hundred', name: '0-100 km/h (s)' },
-            { key: 'weight_distribution', name: 'Weight Distribution' },
-            { key: 'plb_ratio', name: 'Power-to-Weight Ratio' },
-            { key: 'group_type', name: 'Group Type' }
-        ];
-       
-        res.render('result', {
-            carname1: `${marka1} ${model1}`,
-            carname2: `${marka2} ${model2}`,
-            car1: car1,
-            car2: car2,
-            features: features
-
-        });
-    } catch (error) {
-        next(error); 
-    }
-});
-*/
-
-route.get('/admin', (req, res) => {
-  try{
-res.render('Admin');
-  }
-  catch(error){
-    next(error); 
-  }
-    
-});
-
-
-route.get('/api/total-comparisons', async (req, res) => {
-    try {
-        const totalComparisons = await userrepository.Toplam_Karsilastirma_Miktari(); // await eklenmeli!
-        
-
-        // JSON formatında düzgün döndür
-        res.json({ totalComparisons: totalComparisons || 0 });
-    } catch (error) {
-        next(error); 
-    }
-});
-
-
-
-route.get('/api/comparison-data', async (req, res) => {
-    try {
-        const comparisonData = await userrepository.İstatistik_Getir(); // await eklenmeli
-        
-
-        res.json(comparisonData); // Direkt veriyi JSON olarak döndür
-    } catch (error) {
-        next(error); 
-    }
-});
-
-
-route.get('/api/visitor-count',async (req, res) => {
-    const { startDate, endDate } = req.query;
-    console.log(startDate,endDate);
-    if (!startDate || !endDate) {
-        return res.status(400).json({ error: 'Başlangıç ve bitiş tarihi gerekli' });
-    }
-
-    totalVisitors = await userrepository.getVisitorCountBetweenDates(startDate, endDate);
-    console.log("Ziyaretçi Sayısı:", totalVisitors);
-   
-
-    res.json({ visitorCount: totalVisitors });
-});
-
-
-
-
+//Seçilen arabaların sonuçlarının görüntülendiği kısım
 route.get('/car_compare/resultgpt', async (req, res,next) => {
     
     try {
